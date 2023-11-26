9dfacf979ac4b598eda50ad30b51eb4af6f62799
JIS <isjoung5@gmail.com>
2023-11-26 15:25:34 +0900

    20231126-1525 배포 테스트

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,3 @@
-<h1>Welcome to SvelteKit 2023-11-26</h1>
+<h1>Welcome to SvelteKit 2023-11-26 15:24</h1>
 <h1>GitHub Actions Test</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
